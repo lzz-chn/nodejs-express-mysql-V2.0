985ae1fea6d9e783041504bf45f601cc7ec507d3
lzz <lizhongze.chn@gmail.com>
2019-06-16 20:20:54 +0800

    增加存放公共变量与函数的js文件

---
+++ public/js/public.js
@@ -0,0 +1,14 @@
+/**
+ * 获取所属栏目复选框中的数据并添加进复选框
+ * select: 选择器
+ * text: 默认显示的文本
+ */
+const getSelectData = (select, text) => {
+    $.ajax({
+        url: '/admin/nav/navSelect',
+        async: false,
+        success: function(data) {
+            select.html(`<option value="0">${text}</option>${data}`);
+        }
+    });
+};
