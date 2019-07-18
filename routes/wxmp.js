309e4b783a30b6d8014d3064e9407492be2a86aa
hobbs <lizhongze.chn@gmail.com>
2019-07-18 14:00:40 +0800

    增加微信 login 接口

--- routes/wxmp.js
+++ routes/wxmp.js
@@ -4,6 +4,7 @@ var path = require('path');
 var upFile = require('filese'); // 文件管理模块
 var upload = upFile('public/uploads/'); //上传文件的 文件目录
 var corsz = require('corsz'); // 跨域模块
+var request = require('request'); // 请求模块
 var mysql = require('mysql'); // MySQL 数据库模块
 var connection = mysql.createConnection({
     host: '127.0.0.1',
@@ -36,5 +37,20 @@ router.post('/imgUpload', upload.single('photos'), (req, res, next) => {
         res.send('imgUpload error');
     }
 });
+
+// 微信登陆测试
+router.post('/login', (req, res, next) => {
+    //request("写请求地址",回调函数(错误，响应，数据))
+    console.log('wechat login');
+    var options = {
+        url: 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code',
+    };
+    request(options, function(error, response, body) {
+        console.log(body); //body就是接受的数据
+        req.session.weChatLogin = JSON.parse(body); // 数据存储到 session
+        res.send(body);
+    });
+    
+}) 
 
 module.exports = router;
